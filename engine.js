9a86eb284f5d2c55b0353e322608751ed953aeb9
Vojislav Grujic <vojislav.grujic@gmail.com>
2016-08-11 23:37:56 +0200

    Improvements that turned out to be the opposite. :)

--- engine.js
+++ engine.js
@@ -15,22 +15,17 @@ function initCalc() {
   display.value = '';
 
   for ( i = 0; i < len; i++ ) {
-    buttons[i].addEventListener('click', function () {
-      // when a button is clicked, only its reference is needed as a parameter
-      action(this);
-    });
+    buttons[i].addEventListener('click', action);
   }
 
 
   // function that does the reading, calculations, and updates of the display
   // parameter 'element' is provided from clicking on the button while 'input' and 'id' from pressing a button
-  function action(element, input, id) {
-    var input = input || element.value,
-        id = id || element.id,
-        displayValue = display.value,
-        i, len = displayValue.length;
-
-    console.log('element: ', element);
+  function action() {
+    var input = this.value,
+        id = this.id,
+        displayValue,
+        i, len = display.value.length;
 
     // this is the 'Clear' button action
     // clears the display, the expression and the operator
@@ -44,7 +39,7 @@ function initCalc() {
     // this is the 'Delete' button action
     // removes last input from the display and the operation display when display is empty
     if ( input === 'DEL' ) {
-      display.value = displayValue.slice(0, displayValue.length - 1);
+      display.value = display.value.slice(0, display.value.length - 1);
       if ( !display.value ) {
         operationDisplay.style.display = 'none';
       }
@@ -52,9 +47,17 @@ function initCalc() {
 
     // this button changes the sign of the current display value
     // it needs better implementation
-    if ( id === 'plus-minus' && displayValue ) {
-      display.value = displayValue * (-1);
-      element = null;
+    // on pressing the 'Enter' key, somehow this code is executed, but it shouldn't.
+    // I can't solve this problem
+    if ( id === 'plus-minus' && display.value ) {
+      if ( display.value.charAt(len - 1) !== '*' && display.value.charAt(len - 1) !== '/' && operations.indexOf(display.value.charAt(len - 1)) === -1 ) {
+        display.value = eval(display.value) * (-1);
+        operator = '';
+        operationDisplay.style.display = 'none';
+      } else {
+        // this enables the change of sign for the number that comes after multiplication or division
+        display.value = display.value + '-';
+      }
     }
 
     // determining the current operation
@@ -69,11 +72,11 @@ function initCalc() {
       operationDisplay.style.display = 'block';
     }
 
-    // when the input is a number or a dot, several rules must be satisfied
-    if ( input.match(number) ) {
+    // when the input is a number or a dot or an operator, several rules must be satisfied
+    if ( input.match(number) || operator ) {
 
       // if number starts with zero, then dot or operator must follow it
-      if ( displayValue.charAt(0) === '0' &&
+      if ( display.value.charAt(0) === '0' &&
            len === 1 &&
            input.match(/[0-9]/)
          ) return;
@@ -81,8 +84,8 @@ function initCalc() {
       i = len - 1;
       // don't allow entering numbers if last number was zero and there is an operator in front of it
       if ( input.match(/[0-9]/) &&
-           displayValue.charAt(i) === '0' &&
-           operations.indexOf(displayValue.charAt(i - 1)) !== -1
+           display.value.charAt(i) === '0' &&
+           operations.indexOf(display.value.charAt(i - 1)) !== -1
          ) return;
 
       // no more than 14 characters
@@ -92,95 +95,102 @@ function initCalc() {
       if ( input === '.' ) {
         // if last input is not an operator, go back character by character while they match a number
         // if the character matches a dot, then do not allow inserting another one
-        while ( !operator && displayValue.charAt(i).match(number) ) {
-          if ( displayValue.charAt(i) === '.' ) return;
+        while ( !operator && display.value.charAt(i).match(number) ) {
+          if ( display.value.charAt(i) === '.' ) return;
           i--;
         }
       }
 
-      // finally the input can be displayed in the display
-      if ( displayValue && operator ) {
-        display.value = displayValue + operator + input;
-        operator = '';
-      } else {
-        display.value = displayValue + input;
+      // no successive operators allowed, if another operator is pressed, then change previous
+      if ( operations.indexOf(display.value.charAt(len - 1)) !== -1 && operator ) {
+        console.log(display.value.substr(0, len - 1));
+        display.value = display.value.substr(0, len - 1) + operator;
+        return;
       }
+
+      // finally the input can be displayed in the display
+      if ( display.value && operator ) {
+        display.value = display.value + operator;
+      } else {
+        display.value = display.value + input;
+      }
+      operator = '';
+      id = '';
 
     }
 
     // this evaluates the expression
     if ( input === '=' ) {
       // don't do anything if the display/expression is empty
-      if ( !displayValue ) return;
-
-      console.log('display.value 1: ', display.value);
-
-      console.log('displayValue 1: ', displayValue);
+      if ( !display.value ) return;
 
       // if there is something in the display, evaluate it
-      displayValue = eval(display.value);
+      display.value = eval(display.value);
 
-      if ( displayValue.toString().length >= 12 ) {
+      if ( display.value.toString().length >= 12 ) {
         // this prevents displaying large number of decimal places
         // it appears that it also prevents miscalculations that result in things like (0.1 + 0.2 != 0.3 )or (0.3 + 0.6 != 0.9)
-        displayValue = (displayValue * 1).toExponential(4);
+        display.value = (display.value * 1).toExponential(4);
       }
 
-      console.log('display.value 2: ', display.value);
-
-      console.log('displayValue 2: ', displayValue);
-
       // update the display
-      display.value = displayValue;
+      display.value = display.value;
       operationDisplay.style.display = 'none';
       // reset the operator variable
       operator = '';
-
-      console.log('display.value 3: ', display.value);
-
-      console.log('displayValue 3: ', displayValue);
 
     }
 
   }
 
-  // keyboard control
   window.addEventListener('keydown', function(e) {
-    var keyID = e.key,
-        value;
+    // prevent firefox quick search
+    if ( e.key === '/' ) e.preventDefault();
+  });
 
-    console.log('key: ', keyID);
+  // keyboard control
+  window.addEventListener('keyup', function(e) {
+    var keyID = e.key,
+        value,
+        matched = false;
 
     // mapping keys to values and IDs for the elements
     switch (keyID) {
       case '/':
-        // prevent firefox quick search
-        e.preventDefault();
         value = '÷';
+        matched = true;
         break;
       case '*':
         value = '×';
+        matched = true;
         break;
       case 'Enter':
         value = '=';
+        matched = true;
         break;
       case 'Backspace':
         value = 'DEL';
+        matched = true;
         break;
       case 'Delete':
         value = 'C';
+        matched = true;
         break;
-      default:
-        value = keyID;
+      case ('`'):
+        value = '±'
+        matched = true;
         break;
     }
 
-    // PREPARING FOR MAKING A VISUAL EFFECT OF PRESSING A KEY WHEN KEYBOARD IS USED FOR INPUT
+    if ( !matched ) {
+      if ( keyID.match(/[0-9]|\+|-|\./) ) {
+        value = keyID;
+      } else return;
+    }
+
     var button = document.querySelector('input[value="' + value + '"]');
     // console.log(button);
-
-    // when keyboard is used for input, we must pass the value and id that we mapped in the switch statement
-    action(null, value, keyID);
+    button.click();
 
   });
 
