474d9eb3fe85d9ef58b0546af610e95023cfd7b6
Vojislav Grujic <vojislav.grujic@gmail.com>
2016-08-12 21:20:36 +0200

    Improve calculator functionality and display.
    There is still a problem when user mixes the input from the keyboard and the mouse.
    So far there is no solution for that problem so it is best to hold on to one style of input during the session.

--- engine.js
+++ engine.js
@@ -1,8 +1,7 @@
 window.addEventListener('load', initCalc);
 
 function initCalc() {
-  var startPattern = /\d|\.|-/, // expression can only start with number, dot or minus sign
-      numberPattern = /\d|\./, // match valid number characters
+  var numberPattern = /\d|\./, // match valid number characters
       operationPattern = /\+|-|\*|\//, // four arithmetic operations
       number = document.querySelectorAll('.number'), // numbers and a dot
       operation = document.querySelectorAll('.operation'), // operation characters
@@ -10,64 +9,76 @@ function initCalc() {
       signToggle = document.querySelector('#plus-minus'), // changes the sign of the display value
       operationDisplay = document.querySelector('#operation-display'), // element that displays current operation
       calcDisplay = document.querySelector('#display'), // the calculator display element
-      expression = '', // holds the string from the display MAYBE UNDEEDED
       i, len, // iteration variables
       operationISymbol = {'*': '×', '/': '÷', '+': '+', '-': '-'}; // html symbols for operations
 
   // Empty display on reload
   display.value = '';
 
-  window.addEventListener('keydown', function(e) {
-    // prevent firefox quick search
-    if ( e.key === '/' ) e.preventDefault();
-  });
-
-  // keyboard control
-  window.addEventListener('keyup', function(e) {
-    var key = e.key.toLowerCase(),
-        value,
-        matched = false,
-        button;
-
+  function getKeyValue(key) {
     // mapping keys to values and IDs for the elements
     switch (key) {
       case '/':
-        value = '÷';
-        matched = true;
-        break;
+        return '÷';
       case '*':
-        value = '×';
-        matched = true;
-        break;
+        return '×';
       case 'enter':
-        value = '=';
-        matched = true;
-        break;
+        return '=';
       case 'backspace':
-        value = 'DEL';
-        matched = true;
-        break;
+        return 'DEL';
       case 'delete':
-        value = 'C';
-        matched = true;
-        break;
+        return 'C';
       case ('`'): // CHANGE THIS TO BE WITH BUTTON MODIFIER 'Ctrl + `-`'
-        value = '±'
-        matched = true;
-        break;
+        return '±'
+      default:
+        return '';
     }
+  }
+
+  function getButton(key) {
+    var value,
+        button;
+
+    // get proper value for the element
+    value = getKeyValue(key);
 
     // if the pressed key does not match any of the keys we are interested in, then do nothing
-    if ( !matched ) {
-      if ( key.match(/[0-9]|\+|-|\./) ) {
+    if ( !value ) {
+      if ( key.match(/\d|\+|-|\./) ) {
         value = key;
       } else return;
     }
 
     // select appropriate button and fire a click event on it
     button = document.querySelector('input[value="' + value + '"]');
-    console.log(button);
-    // button.click();
+    return button;
+  }
+
+  window.addEventListener('keydown', function(e) {
+    var key = e.key.toLowerCase(),
+        button = getButton(key); // find to what button the 'key' corresponds
+
+    // prevent firefox quick search
+    if ( key === '/' ) e.preventDefault();
+
+    if ( !button ) return;
+
+    // this class adds highlight on the button when the keyboard is used for input
+    button.classList.add('keyboard-active');
+
+  });
+
+  // keyboard control
+  window.addEventListener('keyup', function(e) {
+    var key = e.key.toLowerCase(),
+        button = getButton(key); // find to what button the 'key' corresponds
+
+    if ( !button ) return;
+
+    // simulate click on the button
+    button.click();
+    // after releasing the key, remove the highlight from the button
+    button.classList.remove('keyboard-active');
 
   });
 
@@ -96,8 +107,8 @@ function initCalc() {
 
       // this evaluates the expression
       if ( control === '=' ) {
-        // don't do anything if the display/expression is empty
-        if ( !expression ) return;
+        // don't do anything if the display/expression is empty or the last character is not a digit
+        if ( !expression || !expression.charAt(i).match(/\d/) ) return;
 
         // if there is something in the display, evaluate it
         expression = eval(expression);
@@ -138,20 +149,20 @@ function initCalc() {
       //if number starts with zero, don't allow entering other numbers
       if ( expression.charAt(0) === '0' &&
            len === 1 &&
-           number.match(/[0-9]/)
+           number.match(/\d/)
          ) return;
 
       // don't allow entering numbers if last number was zero and there is an operator in front of it
-      if ( number.match(/[0-9]/) &&
+      if ( number.match(/\d/) &&
            expression.charAt(i) === '0' &&
            expression.charAt(i - 1).match(operationPattern)
        ) return;
 
       // no multiple dots allowed in numbers
       if ( number === '.' ) {
-        // if last character in expression is not an operator, go back character by character while they match a number
+        // go back character by character while they match a number
         // if the character matches a dot, then do not allow inserting another one
-        while ( !expression.charAt(i).match(operationPattern) && expression.charAt(i).match(numberPattern) ) {
+        while ( expression.charAt(i).match(numberPattern) ) {
           if ( expression.charAt(i) === '.' ) return;
           i--;
         }
@@ -182,6 +193,10 @@ function initCalc() {
 
       // if the expression starts with minus and it is the only character in the expression don't enter operations
       if ( expression.charAt(0) === '-' && len === 1 ) return;
+
+      // if there are already two operators present do not add more
+      // this occurs when we are dividing and multiplying with negative numbers
+      if ( expression.charAt(i).match(operationPattern) && expression.charAt(i - 1).match(operationPattern) ) return;
 
       // no successive operators allowed, if another operator is pressed, then change previous
       if ( expression.charAt(i).match(operationPattern) ) {
@@ -217,7 +232,19 @@ function initCalc() {
     // no empty expression allowed and no minus sign allowed after a dot
     if ( !expression || expression.charAt(i) === '.' ) return;
 
-    // if ( !expression.charAt(i - 1).match(operationPattern) ) {
+    // if a minus is only character in the expression remove it
+    if ( expression.charAt(0) === '-' && len === 1 ) {
+      // remove minus
+      calcDisplay.value = '';
+      operationDisplay.textContent = '';
+      operationDisplay.style.display = 'none';
+      return;
+    }
+
+    // these are the cases when we want to make the number negative or back to positive
+    // if the re is already a sign, then according to it either change it or add minus
+    // minus is added only after '*' and '/', otherwise, the sign is changed
+    if ( !expression.charAt(i - 1).match(operationPattern) ) {
       if ( expression.charAt(i) === '-' ) {
         // change minus into plus
         calcDisplay.value = expression.substr(0, i) + '+';
@@ -228,27 +255,21 @@ function initCalc() {
         calcDisplay.value = expression.substr(0, i) + '-';
         operationDisplay.textContent = '-';
         return;
+      } else {
+        // add minus
+        calcDisplay.value = expression + '-';
+        return;
       }
-    // }
-
-    calcDisplay.value = expression + '-';
+    } else {
+      // if we have two operation signs, then it is the combination '*-' or '/-'
+      // so, we will remove minus from the expression
+      if ( expression.charAt(i) === '-' ) {
+        // remove minus
+        calcDisplay.value = expression.substr(0, i);
+        return;
+      }
+    };
 
   });
 
 }
-
-
-//     // this button changes the sign of the current display value
-//     // it needs better implementation
-//     // on pressing the 'Enter' key, somehow this code is executed, but it shouldn't.
-//     // I can't solve this problem
-//     if ( id === 'plus-minus' && display.value ) {
-//       if ( display.value.charAt(len - 1) !== '*' && display.value.charAt(len - 1) !== '/' && operations.indexOf(display.value.charAt(len - 1)) === -1 ) {
-//         display.value = eval(display.value) * (-1);
-//         operator = '';
-//         operationDisplay.style.display = 'none';
-//       } else {
-//         // this enables the change of sign for the number that comes after multiplication or division
-//         display.value = display.value + '-';
-//       }
-//     }
